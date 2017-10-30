c4e767c1b9b70ab06569bd0d86650e9886d15ec9
Levi Lentz <llentz@mit.edu>
2017-10-30 16:34:52 -0400

    Added routine to save gvectors

--- src/Potential.cpp
+++ src/Potential.cpp
@@ -44,6 +44,7 @@
 #include <fstream>
 #include <iomanip>
 #include <algorithm>
+#include <valarray>
 
 #include "NN.h"
 #include "NF.h"
@@ -101,13 +102,26 @@ Potential::Potential(vector<System*> systems_in, Functional_params F_in) : syste
         cout << endl;
       }
     }
-   
+    vector < vector <REAL> > g_vectors;
     if (!systems[0]->structure.is_initialized()) {
       for (int i_sys=0; i_sys<systems.size(); i_sys++) {
 	systems[i_sys]->properties.set_inputs(systems[i_sys]->structure.init_G(&params));
+        for (int i_v=0; i_v < systems[i_sys]->structure.G.size(); i_v++) {
+            g_vectors.push_back(systems[i_sys]->structure.G[i_v]);
+        }
       }
     }
-    
+    stringstream ss; 
+    ss << mpi->rank() << ".gvector";
+    ofstream gvectors;
+    gvectors.open(ss.str().c_str());
+    for (int i_ = 0; i_ < g_vectors.size() ; i_++) {
+        for (int j = 0; j < g_vectors.at(i_).size(); j++) {
+            gvectors << g_vectors[i_][j] << ",";
+        }
+        gvectors << "\n";
+    }
+    gvectors.close();
     
     params.current_atom_type = atom.atomic_symbol();
     
